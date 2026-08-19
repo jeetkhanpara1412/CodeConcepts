ba76afc4784fed4496a662bff927a18b2aae63ed
jeet <jeetkhanpara1412@gmail.com>
2026-08-19 21:56:23 +0530

    Add code concepts

--- Arrays/Second_Largest_Element.py
+++ Arrays/Second_Largest_Element.py
@@ -5,35 +5,40 @@
 
 # 1: Using sort() (Easy)
 
-numbers = [4, 1, 7, 3, 7, 2, 5, 4]
-sorted_numbers = list(set(numbers))  # Remove duplicates and convert back to list
-sorted_numbers.sort()  # Sort the list in ascending order
-print("Second largest is = ", sorted_numbers[-2])
+numbers = [4, 1, 9, 3, 8, 2, 5, 4]
+numbers.sort()
+print("Second largest is =", numbers[-2])
+
+
+# numbers = [4, 1, 9, 3, 8, 2, 5, 4]
+# sorted_numbers = list(set(numbers))  # Remove duplicates and convert back to list
+# sorted_numbers.sort()  # Sort the list in ascending order
+# print("Second largest is = ", sorted_numbers[-2])
 
 
 
 # 2: Without Sorting (Best for Interviews)
 
-numbers = [4, 1, 7, 3, 7, 2, 5, 4]
-largest = second_largest = float('-inf')  # Initialize to negative infinity
+# numbers = [4, 1, 7, 3, 7, 2, 5, 4]
+# largest = second_largest = float('-inf')  # Initialize to negative infinity
 
-for i in numbers:
-    if i > largest:
-        second_largest = largest
-        largest = i
-    else:
-        if largest > i > second_largest:
-            second_largest = i
+# for i in numbers:
+#     if i > largest:
+#         second_largest = largest
+#         largest = i
+#     else:
+#         if largest > i > second_largest:
+#             second_largest = i
 
 
-print("Second largest is = ", second_largest)
+# print("Second largest is = ", second_largest)
 
 
 # 3: Using max()
-numbers = [10, 20, 5, 8, 20, 15]
-largest = max(numbers)
+# numbers = [10, 20, 5, 8, 20, 15]
+# largest = max(numbers)
 
-numbers.remove(largest)
-second_largest = max(numbers)
+# numbers.remove(largest)
+# second_largest = max(numbers)
 
-print("second_largest is = ", second_largest)
+# print("second_largest is = ", second_largest)
